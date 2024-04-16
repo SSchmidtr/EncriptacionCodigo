76e25b0e8a23b52e0ade1e38091a1ec9229dad07
SSchmidtr <sschmidtro@gmail.com>
2024-04-15 19:10:29 -0600

    Primeros pasos en app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 import streamlit as st
+from tools.sha1 import SHA1
+
+
+st.set_page_config(
+    page_title="Encriptación de .exe",
+    page_icon="🔒",
+    layout="wide"
+)
+st.title('Encriptación de .exe')
